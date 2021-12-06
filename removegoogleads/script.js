76884240e7a68987d05959e6223fc8a53b13aa32
TomAPU <44659204+TomAPU@users.noreply.github.com>
2021-12-06 21:16:58 +0800

    Google去广告油猴脚本

---
+++ removegoogleads/script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Google去广告
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  去掉Google的广告的油猴脚本
+// @author       TomAPU
+// @include      https://www.google.*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+while(1){document.getElementsByClassName("cUezCb luh4tb O9g5cc uUPGi")[0].remove()}
+})();
